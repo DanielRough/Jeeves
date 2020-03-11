5b2f544dc2e771b52cdd9ce6231d22c276c806c6
Daniel John Rough <danielrough@Daniels-MacBook-Pro.local>
2020-03-11 21:08:24 +0000

    Fixed names

--- src/main/java/com/jeeves/vpl/Main.java
+++ src/main/java/com/jeeves/vpl/Main.java
@@ -128,10 +128,7 @@ public class Main extends Application {
 	}
 	public Main() {
 	}
-<<<<<<< HEAD
-=======
 
->>>>>>> 0dfeb6ef9decc426458bcb76a973886432aa0e7e
 	public Main(String[] args) {
 		launch(args);
 	}
--- src/main/java/com/jeeves/vpl/Newmain.java
+++
@@ -1,8 +0,0 @@
-package com.jeeves.vpl;
-
-public class NewMain {
-
-	public static void main(String[] args) {
-	new Main(args);
-	}
-}
